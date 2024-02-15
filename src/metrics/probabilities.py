1bde33fdcb66e4bc1a74ecc61e9dfcdf668644af
JoaoArtur <uc2019217853@student.uc.pt>
2024-02-15 16:44:53 +0000

    Fix error in metrics

--- src/metrics/probabilities.py
+++ src/metrics/probabilities.py
@@ -8,15 +8,5 @@ import pandas as pd
 
 
 def joint_probability(data: pd.DataFrame, variables: dict) -> float:
-
-    filtered_data = data.copy()
-    for var, value in variables.items():
-        filtered_data = filtered_data[filtered_data[var] == value]
-
-    n_total = len(data)
-    n_target = len(filtered_data)
-
-    try:
-        return n_total * 1.0 / n_target
-    except ZeroDivisionError:
-        return 1.0
+    probs = data.value_counts(list(variables.keys()), normalize=True)
+    return probs[tuple(variables.values())] if tuple(variables.values()) in probs else 0.0
