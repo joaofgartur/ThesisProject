a263bafde92a165a1e68736efc92a529a96375b9
JoaoArtur <uc2019217853@student.uc.pt>
2024-05-01 18:20:51 +0100

    Add scripts for results processing

--- src/results_processing_scripts/compute_agregated_metrics.py
+++ src/results_processing_scripts/compute_agregated_metrics.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 
-def compute_mean_csv(folder_path, output_file):
+def compute_agregated_metrics(folder_path, output_file):
     # Find the first CSV file in the folder
     first_file_path = None
     for filename in os.listdir(folder_path):
@@ -28,15 +28,26 @@ def compute_mean_csv(folder_path, output_file):
 
             # Store values for each numeric column
             for col in col_values.keys():
-                col_values[col] = pd.concat([col_values[col].reset_index(drop=True), df[col].reset_index(drop=True)], axis=1)
+                col_values[col] = pd.concat([col_values[col], df[col]], axis=1)
 
     for col in col_values.keys():
-        mean_values = np.round(col_values[col].mean(axis=1, skipna=True), decimals=4)
-        col_values[col].iloc[:, 0] = mean_values
-        col_values[col] = col_values[col].iloc[:, :1]
+        # mean_values = np.round(col_values[col].mean(axis=1, skipna=True), decimals=4)
+        sum_square_distances = np.round(((col_values[col].sub(1.0, axis=0)) ** 2).sum(axis=1), decimals=4)
 
-    for col in col_values:
-        final_df[col] = col_values[col]
+        # Create a DataFrame with mean values and sum of square distances
+        new_df = pd.DataFrame({
+            # f'{col}_mean': mean_values,
+            f'{col}_ssd': sum_square_distances
+        }, index=final_df.index)
+
+        # Get the index position of the current column in final_df
+        index_position = final_df.columns.get_loc(col)
+
+        # Drop the original column from final_df
+        final_df = final_df.drop(columns=col)
+
+        # Concatenate new_df with final_df at the corresponding index position
+        final_df = pd.concat([final_df.iloc[:, :index_position], new_df, final_df.iloc[:, index_position:]], axis=1)
 
     final_df.to_csv(output_file)
 
@@ -45,6 +56,6 @@ if __name__ == '__main__':
     algorithms = ['Massaging', 'Reweighing', 'DisparateImpactRemover', 'AIF360LearningFairRepresentations', 'LGAFFS', 'PermutationGeneticAlgorithm']
 
     for algorithm in algorithms:
-        input_folder = f"results_german_27_04_2024/{algorithm}"
+        input_folder = f"results_german_28_04_2024/{algorithm}"
         output_file = f"{input_folder}/{algorithm}_mean_values.csv"
-        compute_mean_csv(input_folder, output_file)
+        compute_agregated_metrics(input_folder, output_file)
