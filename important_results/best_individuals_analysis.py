dab704ce302b0979ad322752fba7ac4fd7792c5d
JoaoArtur <uc2019217853@student.uc.pt>
2024-08-23 12:42:31 +0100

    Update results processing

--- important_results/best_individuals_analysis.py
+++ important_results/best_individuals_analysis.py
@@ -2,6 +2,10 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+"""
+### Plots
+"""
+
 for patch in plt.gca().patches:
     current_width = patch.get_width()
     patch.set_width(current_width * 0.8)  # Reduce the width by 20%
@@ -108,7 +112,12 @@ def bar_plot(df, x_axis, y_axis, title, hue=None):
     if show_plot:
         plt.show()
 
-def compute_global_df(base_directory, base_seed, num_runs, dataset, attribute, head_size=0, tail_size=0):
+
+"""
+### Aggregate metrics computation
+"""
+
+def build_global_df(base_directory, base_seed, num_runs, dataset, attribute, head_size=0, tail_size=0):
 
     global_df = pd.DataFrame()
     for seed in range(base_seed, base_seed + num_runs):
@@ -147,6 +156,9 @@ def frequency_analysis(global_df):
     frequency = {k: v for k, v in sorted(frequency.items(), key=lambda item: item[1], reverse=True)}
     frequency_df = pd.DataFrame(frequency.items(), columns=['Genotype', 'Frequency'])
 
+    mapped_genotypes, frequency_df = map_genotypes(frequency_df)
+    mapped_genotypes.to_latex(f'{dataset}_mapped_genotypes.tex', index_names=False, index=False)
+
     return frequency_df
 
 
@@ -173,7 +185,7 @@ def compute_average(df):
     return result
 
 
-def compute_agregated_metrics(directory, dataset, attribute, base_seed, num_runs, head_size=0):
+def compute_aggregated_metrics(directory, dataset, attribute, base_seed, num_runs, head_size=0):
 
     first_file = f'{directory}/{dataset}/FairGenes_{attribute}_fitness_evolution_seed_{base_seed}.csv'
     final_df = pd.read_csv(first_file, index_col=0)
@@ -200,7 +212,7 @@ def compute_agregated_metrics(directory, dataset, attribute, base_seed, num_runs
             continue
 
 
-    fairness_df = compute_sum_square_distances(fairness_df)
+    fairness_df = compute_average(fairness_df)
     performance_df = compute_average(performance_df)
 
     final_df = pd.concat([final_df, fairness_df, performance_df], axis=1)
@@ -215,41 +227,57 @@ def best_vs_population_analysis(global_df, classifiers, metrics, metric_type='fa
     metrics_df = {}
 
     classifier_mapping = {
-        'LogisticRegression': 'LR',
-        'SVC': 'SVM',
-        'GaussianNB': 'NB',
-        'DecisionTreeClassifier': 'DT',
-        'RandomForestClassifier': 'RF'
+        'LogisticRegression': 'Logistic Regression',
+        'SVC': 'Support Vector Machine',
+        'GaussianNB': 'Gaussian Naive Bayes',
+        'DecisionTreeClassifier': 'Decision Tree',
+        'RandomForestClassifier': 'Random Forest'
     }
 
-    for metric in metrics:
-        metric_df = pd.DataFrame()
+    metric_mapping = {
+        'disparate_impact': 'DI',
+        'discrimination_score': 'DS',
+        'true_positive_rate_diff': 'TPD',
+        'false_positive_rate_diff': 'FPD',
+        'false_positive_error_rate_balance_score': 'FPE',
+        'false_negative_error_rate_balance_score': 'FNE',
+        'consistency': 'CON'
+    }
 
-        for classifier in classifiers:
-            best_df = global_df[[f'{classifier}_{metric_type}_{metric}']].rename(columns={f'{classifier}_{metric_type}_{metric}': f'Best_{classifier_mapping[classifier]}'})
-            mean_df = global_df[[f'{classifier}_{metric_type}_{metric}mean']].rename(columns={f'{classifier}_{metric_type}_{metric}mean': f'Population_{classifier_mapping[classifier]}'})
+    for classifier in classifiers:
+        df = pd.DataFrame()
+
+        for metric in metrics:
+            best_df = global_df[[f'{classifier}_{metric_type}_{metric}']].rename(columns={f'{classifier}_{metric_type}_{metric}': f'Best_{metric_mapping[metric]}'})
+            mean_df = global_df[[f'{classifier}_{metric_type}_{metric}mean']].rename(columns={f'{classifier}_{metric_type}_{metric}mean': f'Population_{metric_mapping[metric]}'})
 
             mean_df = mean_df.reset_index(drop=True)
             best_df = best_df.reset_index(drop=True)
 
-            metric_df = pd.concat([metric_df, best_df, mean_df], axis=1)
-            # metric_df = metric_df.set_index('Generation')
-            metric_df.reset_index(drop=True, inplace=True)
-            generation = range(len(metric_df))
-            metric_df['Generation'] = generation
-            metric_df.set_index('Generation', inplace=True)
+            df = pd.concat([df, best_df, mean_df], axis=1)
+            df.reset_index(drop=True, inplace=True)
+            generation = range(len(df))
+            df['Generation'] = generation
+            df.set_index('Generation', inplace=True)
 
-        metrics_df[metric] = metric_df
+        metrics_df[classifier] = df
+
+    metrics_df = {classifier_mapping[k]: v for k, v in metrics_df.items()}
 
     return metrics_df
 
 
 if __name__ == '__main__':
 
+    #dataset = 'Law School Admission Bar Passage'
+    #attribute = 'race1'
+
+    dataset = 'German Credit'
+    attribute = 'Attribute9'
+
+
     base_seed = 42
     num_runs = 30
-    dataset = 'Law School Admission Bar Passage'
-    attribute = 'race1'
     base_directory = 'best_individuals'
     classifiers = ['LogisticRegression', 'SVC', 'GaussianNB', 'DecisionTreeClassifier', 'RandomForestClassifier']
     metrics = ['disparate_impact', 'discrimination_score', 'true_positive_rate_diff', 'false_positive_rate_diff',
@@ -260,25 +288,21 @@ if __name__ == '__main__':
 
     show_plot = False
 
-    global_df = compute_global_df(base_directory, base_seed, num_runs, dataset, attribute, head_size=head_size, tail_size=tail_size)
+    global_df = build_global_df(base_directory, base_seed, num_runs, dataset, attribute, head_size=head_size, tail_size=tail_size)
     frequency_df = frequency_analysis(global_df)
-    mapped_genotypes, frequency_df = map_genotypes(frequency_df)
-    mapped_genotypes.to_latex(f'{dataset}_mapped_genotypes.tex', index_names=False, index=False)
     bar_plot(frequency_df, title=f'Genotype Frequency Analysis for {dataset}', x_axis='Genotype', y_axis='Frequency')
 
-
-    aggregated_df = compute_agregated_metrics(base_directory, dataset, attribute, base_seed, num_runs, head_size)
+    aggregated_df = compute_aggregated_metrics(base_directory, dataset, attribute, base_seed, num_runs, head_size)
     metrics_dfs = best_vs_population_analysis(aggregated_df, classifiers, metrics)
 
-    for metric, df in metrics_dfs.items():
-        df_melted = df.melt(var_name='Classifier', value_name='Performance')
-        df_melted['Type'] = df_melted['Classifier'].apply(lambda x: 'Best' if 'Best' in x else 'Population')
-        df_melted['Classifier'] = df_melted['Classifier'].apply(
-            lambda x: x.replace('Best_', '').replace('Population_', ''))
+    for classifier, df in metrics_dfs.items():
+        df_melted = df.melt(var_name='Metric', value_name='Average Error')
+        df_melted['Legend'] = df_melted['Metric'].apply(lambda x: 'Best' if 'Best' in x else 'Population')
+        df_melted['Metric'] = df_melted['Metric'].apply(lambda x: x.replace('Best_', '').replace('Population_', ''))
 
         if line:
             df_melted['Generation'] = df_melted.index % 30
-            line_plot(df_melted, title=f'{dataset} - {metric}', x_axis='Generation', y_axis='Performance', hue='Classifier', style='Type')
+            line_plot(df_melted, title=f'{dataset} - {classifier}', x_axis='Generation', y_axis='Average Error', hue='Metric', style='Legend')
         else:
-            cat_plot(df_melted, title=f'{dataset} - {metric}', x_axis='Classifier', y_axis='Performance', hue='Type')
+            cat_plot(df_melted, title=f'{dataset} - {classifier}', x_axis='Metric', y_axis='Average Error', hue='Legend')
 
--- important_results/metrics_analysis.py
+++ important_results/metrics_analysis.py
@@ -4,6 +4,111 @@ DI_SSE_LIMIT = 1.2
 FAIRNESS_SSE_LIMIT = 0.075
 
 
+"""
+### Aggregate metrics computation
+"""
+
+def compute_sum_square_error(df):
+    metrics = df.columns.unique()
+
+    result = pd.DataFrame()
+    for col in metrics:
+        metric_columns = df[col]
+        sum_square_distances = ((metric_columns.sub(1.0, axis=0)) ** 2).sum(axis=1)
+        result[col] = sum_square_distances
+
+    return result
+
+def compute_agregated_metrics(directory, dataset, attribute, base_seed, num_runs, num_decimals=3):
+
+    # get the columns
+    first_file = f'{directory}/{dataset}_{base_seed}_{attribute}.csv'
+    final_df = pd.read_csv(first_file, index_col=0)
+
+    # get fairness and performance columns
+    fairness_columns = [col for col in final_df.columns if 'fairness' in col]
+    performance_columns = [col for col in final_df.columns if 'performance' in col]
+
+    # keep metadata columns
+    final_df = final_df.drop(columns=fairness_columns + performance_columns)
+    fairness_df = pd.DataFrame()
+    performance_df = pd.DataFrame()
+
+    # compute aggregated metrics
+    for seed in range(base_seed, base_seed + num_runs):
+        try:
+            seed_df = pd.read_csv(f'{directory}/{dataset}_{seed}_{attribute}.csv', index_col=0)
+            fairness_df = pd.concat([fairness_df, seed_df[seed_df.columns.intersection(fairness_columns)]], axis=1)
+            performance_df = pd.concat([performance_df, seed_df[seed_df.columns.intersection(performance_columns)]], axis=1)
+        except FileNotFoundError:
+            continue
+
+    fairness_df = compute_sum_square_error(fairness_df)
+    performance_df = compute_average(performance_df)
+
+    final_df = pd.concat([final_df, fairness_df, performance_df], axis=1)
+    final_df = final_df.round(num_decimals)
+
+    return final_df
+
+
+def compute_average(df):
+    unique_columns = df.columns.unique()
+    result = pd.DataFrame()
+    for col in unique_columns:
+        metric_columns = df[col]
+        average = metric_columns.mean(axis=1)
+        result[col] = average
+
+    return result
+
+
+"""
+### Base tables computation
+"""
+
+
+def pad_decimals(df, num_decimals):
+
+    def format_float(x):
+        if isinstance(x, (float)):
+            return f"{x:.{num_decimals}f}"
+        return x
+
+    df = df.applymap(format_float)
+
+    return df
+
+
+def parse_global_df(global_df):
+    binary_algorithms = ['Massaging', 'Reweighing', 'DisparateImpactRemover', 'LexicographicGeneticAlgorithmFairFeatureSelection']
+    multiclass_algorithms = ['MulticlassLexicographicGeneticAlgorithmFairFeatureSelection', 'FairGenes']
+
+    first_part = global_df[global_df[iterations_column] == 0]
+    first_part.loc[:, 'group'] = first_part['value']
+    binary_part = global_df[(global_df['algorithm'].isin(binary_algorithms)) & (global_df['group'] == global_df['value'])]
+    multiclass_part = global_df[(global_df['algorithm'].isin(multiclass_algorithms))]
+
+    global_df = pd.concat([first_part, binary_part, multiclass_part])
+
+    return global_df
+
+
+def parse_classifiers_df(global_df, exclude_validation=False):
+
+    classifiers = global_df['classification_algorithm'].unique()
+
+    for classifier in classifiers:
+        classifier_table = global_df[global_df['classification_algorithm'] == classifier]
+        classifier_table = classifier_table.drop(columns=['classification_algorithm'])
+
+        if exclude_validation:
+            classifier_table = classifier_table[classifier_table['set'] != 'Validation']
+
+        classifier_table = classifier_table.sort_values(by=['value'], kind='mergesort')
+        classifier_table.to_csv(f'{directory}/{dataset}_{attribute}_{classifier}.csv')
+
+
 def color_fairness_value(value, baseline, is_di):
     formatted_val = f"{value:.3f}"
 
@@ -36,9 +141,9 @@ def colour_analysis(directory, dataset, attribute, classifier):
 
     colored_df = pd.DataFrame()
     for value in protected_groups:
-        baseline = df[(df['value'] == value) & (df['iterations'] == 0)].iloc[0]
+        baseline = df[(df['value'] == value) & (df[iterations_column] == 0)].iloc[0]
 
-        rows_to_color = df[(df['value'] == value) & (df['iterations'] > 0)].copy()
+        rows_to_color = df[(df['value'] == value) & (df[iterations_column] > 0)].copy()
 
         fairness_columns = [col for col in rows_to_color.columns if 'fairness' in col]
         performance_columns = [col for col in rows_to_color.columns if 'performance' in col]
@@ -67,30 +172,35 @@ def multiple_iterations_analysis(directory, dataset, attribute, classifiers):
 
     for classifier in classifiers:
         classifier_df = pd.read_csv(f'{directory}/{dataset}_{attribute}_{classifier}.csv', index_col=0)
+        classifier_ = pd.DataFrame({'classifier': classifier for _ in range(classifier_df.shape[0])}, index=classifier_df.index)
+        classifier_df = pd.concat([classifier_, classifier_df], axis=1)
         df = pd.concat([df, classifier_df])
-    df = df[df['iterations'] > 0]
+    df = df[df[iterations_column] > 0]
 
     results = pd.DataFrame()
     protected_groups = df['value'].unique()
     algorithms = df['algorithm'].unique()
 
-    combinations = [(algorithm, value) for algorithm in algorithms for value in protected_groups]
+    combinations = [(classifier, algorithm, value) for classifier in classifiers for algorithm in algorithms for value in protected_groups]
 
     for comb in combinations:
-        algorithm, value = comb
-        algorithm_df = df[(df['algorithm'] == algorithm) & (df['value'] == value)]
+        classifier, algorithm, value = comb
+        algorithm_df = df[(df['classifier'] == classifier) & (df['algorithm'] == algorithm) & (df['value'] == value)]
         try:
-            first_iteration = algorithm_df[algorithm_df['iterations'] == 1].iloc[0]
-            second_iteration = algorithm_df[algorithm_df['iterations'] == 2].iloc[0]
+            first_iteration = algorithm_df[algorithm_df[iterations_column] == 1].iloc[0]
+            second_iteration = algorithm_df[algorithm_df[iterations_column] == 2].iloc[0]
 
             first_iter_fairness = first_iteration[[col for col in first_iteration.index if 'fairness' in col]]
             second_iter_fairness = second_iteration[[col for col in second_iteration.index if 'fairness' in col]]
 
             fairness_variation = second_iter_fairness - first_iter_fairness
+
             num_increases = (fairness_variation > 0).sum()
             num_decreases = (fairness_variation < 0).sum()
             num_no_change = (fairness_variation == 0).sum()
-            algorithm_df = pd.DataFrame({'algorithm': algorithm, 'group': value, 'num_increases': num_increases, 'num_decreases': num_decreases, 'num_no_change': num_no_change}, index=[0])
+            algorithm_df = pd.DataFrame({'classifier': classifier, 'algorithm': algorithm, 'group': value,
+                                         'num_increases': num_increases, 'num_decreases': num_decreases,
+                                         'num_no_change': num_no_change}, index=[0])
             results = pd.concat([results, algorithm_df])
 
         except IndexError as e:
@@ -113,10 +223,23 @@ def multiple_iterations_analysis(directory, dataset, attribute, classifiers):
 
 if __name__ == '__main__':
     
-    dataset = 'Law School Admission Bar Passage'
-    attribute = 'race1'
+    # dataset = 'Law School Admission Bar Passage'
+    # attribute = 'race1'
+    dataset = 'German Credit'
+    attribute = 'Attribute9'
+    iterations_column = 'iteration' if dataset == 'German Credit' else 'iterations'
+
+
     directory = 'metrics'
+    base_seed = 42
+    num_runs = 30
     classifiers = ['LogisticRegression', 'SVC', 'GaussianNB', 'DecisionTreeClassifier', 'RandomForestClassifier', 'XGBClassifier']
+
+    aggregated_metrics = compute_agregated_metrics(directory, dataset, attribute, base_seed, num_runs)
+
+    global_df = parse_global_df(aggregated_metrics)
+    global_df = pad_decimals(global_df, 3)
+    parse_classifiers_df(global_df, True)
 
     for classifier in classifiers:
         colour_analysis(directory, dataset, attribute, classifier)
